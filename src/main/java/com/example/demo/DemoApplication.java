c21f272fcdb69aad6e0128d22e275b6163e59f4d
masish <masish@users.noreply.github.com>
2022-07-27 11:47:50 +0900

    Say yes

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -22,6 +22,6 @@ public class DemoApplication extends SpringBootServletInitializer {
 	@RequestMapping("/warmup")
 	String sayYes() {
 		Thread.sleep(50000); 
-		return "Hello Yes!";
+		return "Say Yes!";
 	}
 }
